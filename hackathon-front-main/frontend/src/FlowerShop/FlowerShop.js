073f97667d770cfd686ee3fa694537afaeada8f8
Ryuyo <ctwk0123@mail4.doshisha.ac.jp>
2024-06-08 20:31:36 +0900

    いらない入力を削除

--- hackathon-front-main/frontend/src/FlowerShop/FlowerShop.js
+++ hackathon-front-main/frontend/src/FlowerShop/FlowerShop.js
@@ -167,8 +167,6 @@ const style = {
 
 function FlowerShop() {
   const [open, setOpen] = useState(false);
-  const [flower, setFlower] = useState('');
-  const [color, setColor] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [duetime, setDueTime] = useState(null);
@@ -178,7 +176,7 @@ function FlowerShop() {
   const handleClose = () => setOpen(false);
 
   const handleSubmit = async () => {
-    if (!title || !description || !duetime || !flower || !color || !taskType) {
+    if (!title || !description || !duetime || !taskType) {
       alert('You have missing input');
       return;
     }
@@ -188,8 +186,6 @@ function FlowerShop() {
         title,
         description,
         dueDate: new Date(duetime),
-        flower,
-        color,
         flowerImages: 'tulip',
         taskType,
       });
@@ -248,40 +244,6 @@ function FlowerShop() {
             onChange={(e) => setDescription(e.target.value)}
             sx={{ mb: 2 }} 
           />
-          <FormControl variant="standard" fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="flower-label">花の名前</InputLabel>
-            <Select
-              labelId="flower-label"
-              id="flower-select"
-              value={flower}
-              onChange={(e) => setFlower(e.target.value)}
-              label="花の名前"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value="tulip">チューリップ</MenuItem>
-              <MenuItem value="marguerite">マーガレット</MenuItem>
-              <MenuItem value="rose">バラ</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" fullWidth sx={{ mb: 2 }}>
-            <InputLabel id="color-label">色</InputLabel>
-            <Select
-              labelId="color-label"
-              id="color-select"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-              label="色"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value="red">赤</MenuItem>
-              <MenuItem value="blue">青</MenuItem>
-              <MenuItem value="yellow">黄色</MenuItem>
-            </Select>
-          </FormControl>
           <FormControl variant="standard" fullWidth sx={{ mb: 2 }}>
             <InputLabel id="taskType-label">タスクタイプ</InputLabel>
             <Select
